412def4b8ca2d3e76b777cb007c4930434eb4dae
Arif Shahriyar Nader <arif.nader95@gmail.com>
2025-08-25 04:29:06 +0600

    add swagger ui

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,35 @@
 import express, { Application } from "express";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import yaml from "js-yaml";
 import { appConfig } from "./config/appConfig";
 import connectDB from "./db";
-import apiRoutes from './routes/api'
+import apiRoutes from "./routes/api";
 
 const app: Application = express();
+const swaggerDocument = yaml.load(
+  fs.readFileSync("./swagger.yaml", "utf8")
+) as Record<string, any>;
+
 app.use(express.json());
 
 connectDB();
 
-app.use("/api",apiRoutes)
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {}, {
+    explorer: true,
+    docExpansion: "list",
+    operationsSorter: "alpha",
+    tagsSorter: "alpha",
+  })
+);
+
+
+
+app.use("/api", apiRoutes);
 
 app.listen(appConfig.PORT, () => {
   console.log(`Server is running on port ${appConfig.PORT}`);
